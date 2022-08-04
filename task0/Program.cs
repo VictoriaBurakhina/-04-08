845141b7f881399a21c9de63df2f7217c76d5f7c
Victoria <vika.mosenko@yandex.ru>
2022-08-04 20:46:07 +0300

    add task0

---
+++ task0/Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+// 4=16
+int userNumber = new int ();
+Console.WriteLine ("Введите число");
+userNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = userNumber*userNumber;
+Console.WriteLine ($"степень числа {userNumber}={result}");
