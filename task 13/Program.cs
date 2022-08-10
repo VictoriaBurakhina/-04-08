07581b6987b93752e79837611880a47b48cc0e75
Victoria <vika.mosenko@yandex.ru>
2022-08-10 15:53:54 +0300

    Add 13 task

---
+++ task 13/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход число и проверяет, 
+//кратно ли оно одновременно 7 и 23. 14 -> нет 46 -> нет 161 -> да
+
+int number = new int ();
+Console.WriteLine ("Введите число");
+number=Convert.ToInt32(Console.ReadLine());
+int DevisionOnSeven=number%7;
+int DevisionOn23=number%23;
+if (DevisionOnSeven ==0 && DevisionOn23==0)
+{
+    Console.WriteLine ($"да");
+}
+else
+{
+    Console.WriteLine ($"нет");
+}
